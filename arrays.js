f1f6e3aad0ef059d0c6424df27186104db8cf050
Morgan V Hirsch <morganhirsch93@hotmail.co.uk>
2018-02-08 11:53:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,7 +30,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-  array.slice(1)
+  newArray = [...array]
+  newArray.slice(1)
   return array
 }
 
